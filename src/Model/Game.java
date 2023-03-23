09cc7749e609e168a4183485e311f4c3de48c2ff
JohnnyJapes <lhanrahan2@gmail.com>
2023-03-23 17:05:59 -0400

    Rename size to fieldSize to be more verbose placeholder methods for future use get/set methods

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -23,7 +23,7 @@ public class Game {
 
     int p1Score, p2Score, highestScore, winScore;
     //Playing field size, [x,y]
-    int[] size;
+    int[] fieldSize;
 
     // Instance Variables -- define your private data
 
@@ -35,12 +35,171 @@ public class Game {
         ball = new Ball();
         leftPaddle = new Paddle();
         rightPaddle = new Paddle();
-        size = new int[]{1200, 800};
+        fieldSize = new int[]{1200, 800};
         p1Score = 0;
         p2Score = 0;
         winScore = 0;
         highestScore = 0;
     }
 
+    //get methods
 
+    /**
+     * Gets ball.
+     *
+     * @return Model.Ball, value of ball
+     */
+    public Ball getBall() {
+        return ball;
+    }
+
+    /**
+     * Gets leftPaddle.
+     *
+     * @return Model.Paddle, value of leftPaddle
+     */
+    public Paddle getLeftPaddle() {
+        return leftPaddle;
+    }
+
+    /**
+     * Gets rightPaddle.
+     *
+     * @return Model.Paddle, value of rightPaddle
+     */
+    public Paddle getRightPaddle() {
+        return rightPaddle;
+    }
+
+    /**
+     * Gets p1Score.
+     *
+     * @return int, value of p1Score
+     */
+    public int getP1Score() {
+        return p1Score;
+    }
+
+    /**
+     * Gets p2Score.
+     *
+     * @return int, value of p2Score
+     */
+    public int getP2Score() {
+        return p2Score;
+    }
+
+    /**
+     * Gets highestScore.
+     *
+     * @return int, value of highestScore
+     */
+    public int getHighestScore() {
+        return highestScore;
+    }
+
+    /**
+     * Gets winScore.
+     *
+     * @return int, value of winScore
+     */
+    public int getWinScore() {
+        return winScore;
+    }
+
+    /**
+     * Gets fieldSize.
+     *
+     * @return int[], value of size
+     */
+    public int[] getFieldSize() {
+        return fieldSize;
+    }
+
+    //Set Methods
+
+    /**
+     * Method to set ball.
+     *
+     * @param ball Model.Ball - ball
+     */
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    /**
+     * Method to set leftPaddle.
+     *
+     * @param leftPaddle Model.Paddle - leftPaddle
+     */
+    public void setLeftPaddle(Paddle leftPaddle) {
+        this.leftPaddle = leftPaddle;
+    }
+
+    /**
+     * Method to set rightPaddle.
+     *
+     * @param rightPaddle Model.Paddle - rightPaddle
+     */
+    public void setRightPaddle(Paddle rightPaddle) {
+        this.rightPaddle = rightPaddle;
+    }
+
+    /**
+     * Method to set p1Score.
+     *
+     * @param p1Score int - p1Score
+     */
+    public void setP1Score(int p1Score) {
+        this.p1Score = p1Score;
+    }
+
+    /**
+     * Method to set p2Score.
+     *
+     * @param p2Score int - p2Score
+     */
+    public void setP2Score(int p2Score) {
+        this.p2Score = p2Score;
+    }
+
+    /**
+     * Method to set highestScore.
+     *
+     * @param highestScore int - highestScore
+     */
+    public void setHighestScore(int highestScore) {
+        this.highestScore = highestScore;
+    }
+
+    /**
+     * Method to set winScore.
+     *
+     * @param winScore int - winScore
+     */
+    public void setWinScore(int winScore) {
+        this.winScore = winScore;
+    }
+
+    /**
+     * Method to set fieldSize.
+     *
+     * @param size int[] - size
+     */
+    public void setFieldSize(int[] size) {
+        this.fieldSize = size;
+    }
+
+
+    //Methods for Game logic
+
+    public void updatePaddlePosition(int player, char direction) { //handles paddle movement when called by Controller
+
+    };
+
+    public void updateBallPosition(){} //handles tracking of balls next movement
+
+    public void addPoint(int player) {} //add point to selected player
+
+    public void resetPoints() {} //clear the point totals of both players
 }
