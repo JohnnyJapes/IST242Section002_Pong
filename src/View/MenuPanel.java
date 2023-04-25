eba35b422a1b76c271dfa40e9c6fc630f24c2677
BrandonOrlando <orlando.brandon@protonmail.com>
2023-04-25 15:33:06 -0400

    updated menu panel

--- src/View/MenuPanel.java
+++ src/View/MenuPanel.java
@@ -1,5 +1,6 @@
 package View;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.*;
 
@@ -12,20 +13,31 @@ import java.awt.event.*;
  */
 
 public class MenuPanel extends JPanel implements ActionListener {
-        JButton startButton, quitButton;
-        JLabel title;
-        public MenuPanel() {
-            startButton = new JButton("Play Pong");
-            //startButton.addActionListener(e -> new GameFrame());
-            startButton.setLocation(100, 100);
-            startButton.setBounds(100, 100, 100, 100);
-            add(startButton);
-            quitButton = new JButton("Exit");
-            quitButton.addActionListener(e -> System.exit(0));
-            add(quitButton);
-            title = new JLabel("Welcome To Pong!");
-            add(title);
-        }
+    JButton startButton, quitButton;
+    JLabel title;
+    public MenuPanel() {
+        setLayout(new BorderLayout());
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        startButton = new JButton("Play Pong");
+        startButton.setPreferredSize(new Dimension(300, 100));
+        startButton.setForeground(Color.darkGray);
+        startButton.setBackground(Color.lightGray);
+        buttonPanel.add(startButton);
+        quitButton = new JButton("Exit");
+        quitButton.setPreferredSize(new Dimension(300, 100));
+        quitButton.setForeground(Color.darkGray);
+        quitButton.setBackground(Color.lightGray);
+        buttonPanel.add(quitButton);
+        quitButton.addActionListener(e -> System.exit(0));
+        add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.setBackground(Color.DARK_GRAY);
+        title = new JLabel("Welcome To Pong!");
+        title.setFont(new Font("Arial", Font.BOLD, 100));
+        title.setForeground(Color.white);
+        add(title, BorderLayout.NORTH);
+        setBackground(Color.DARK_GRAY);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
